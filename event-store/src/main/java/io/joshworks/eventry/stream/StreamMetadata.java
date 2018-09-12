d067e62b4e3bb9d04801d2bad5e5419e10c05b7d
Josue <josue.eduardo206@gmail.com>
2018-09-13 00:25:12 +0100

    Moved event-store back

---
+++ event-store/src/main/java/io/joshworks/eventry/stream/StreamMetadata.java
@@ -0,0 +1,81 @@
+package io.joshworks.eventry.stream;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StreamMetadata {
+
+    public final String name;
+    public final long hash;
+    public final long created;
+
+    public final long maxAge;
+    public final int maxCount;
+
+    public final int state;
+
+    final Map<String, Integer> permissions;
+    final Map<String, String> metadata;
+
+    public StreamMetadata(String name, long hash, long created) {
+        this.name = name;
+        this.hash = hash;
+        this.created = created;
+        this.maxAge = -1;
+        this.maxCount = -1;
+        this.permissions = new HashMap<>();
+        this.metadata = new HashMap<>();
+        this.state = STREAM_ACTIVE;
+    }
+
+    public StreamMetadata(String name, long hash, long created, long maxAge, int maxCount, Map<String, Integer> permissions, Map<String, String> metadata, int state) {
+        this.name = name;
+        this.hash = hash;
+        this.created = created;
+        this.maxAge = maxAge;
+        this.maxCount = maxCount;
+        this.permissions = permissions;
+        this.metadata = metadata;
+        this.state = state;
+    }
+
+    public boolean hasReadPermission(String id) {
+        return permissions.getOrDefault(id, PERMISSION_NONE).equals(PERMISSION_READ);
+    }
+
+    public boolean hasWritePermission(String id) {
+        return permissions.getOrDefault(id, PERMISSION_NONE).equals(PERMISSION_WRITE);
+    }
+
+    public boolean streamDeleted() {
+        return state == STREAM_DELETED;
+    }
+
+    public boolean streamActive() {
+        return state == STREAM_ACTIVE;
+    }
+
+    public boolean streamLocked() {
+        return state == STREAM_LOCKED;
+    }
+
+    public String metadata(String key) {
+        return metadata.get(key);
+    }
+
+    public String name() {
+        return name;
+    }
+
+
+    //not using enums to easier serialization
+    public static final int PERMISSION_NONE = 0;
+    public static final int PERMISSION_READ = 1;
+    public static final int PERMISSION_WRITE = 2;
+
+    public static final int STREAM_ACTIVE = 0;
+    public static final int STREAM_LOCKED = 1;
+    public static final int STREAM_DELETED = 2;
+
+}
+
