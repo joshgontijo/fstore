f4adda0d914f85ccc91e8bc297cb2c2af3b06b88
Josue <josue.eduardo206@gmail.com>
2018-09-13 01:53:29 +0100

    Added unit tests for the RecordCleanup

--- event-store/src/main/java/io/joshworks/eventry/stream/StreamMetadata.java
+++ event-store/src/main/java/io/joshworks/eventry/stream/StreamMetadata.java
@@ -1,9 +1,25 @@
 package io.joshworks.eventry.stream;
 
+import io.joshworks.eventry.utils.StringUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class StreamMetadata {
+
+
+    //not using enums to easier serialization
+    public static final int PERMISSION_NONE = 0;
+    public static final int PERMISSION_READ = 1;
+    public static final int PERMISSION_WRITE = 2;
+
+    public static final int STREAM_ACTIVE = 0;
+    public static final int STREAM_LOCKED = 1;
+    public static final int STREAM_DELETED = 2;
+
+    public static final int NO_MAX_AGE = -1;
+    public static final int NO_MAX_COUNT = -1;
+
 
     public final String name;
     public final long hash;
@@ -17,17 +33,6 @@ public class StreamMetadata {
     final Map<String, Integer> permissions;
     final Map<String, String> metadata;
 
-    public StreamMetadata(String name, long hash, long created) {
-        this.name = name;
-        this.hash = hash;
-        this.created = created;
-        this.maxAge = -1;
-        this.maxCount = -1;
-        this.permissions = new HashMap<>();
-        this.metadata = new HashMap<>();
-        this.state = STREAM_ACTIVE;
-    }
-
     public StreamMetadata(String name, long hash, long created, long maxAge, int maxCount, Map<String, Integer> permissions, Map<String, String> metadata, int state) {
         this.name = name;
         this.hash = hash;
@@ -38,6 +43,8 @@ public class StreamMetadata {
         this.metadata = metadata;
         this.state = state;
     }
+
+
 
     public boolean hasReadPermission(String id) {
         return permissions.getOrDefault(id, PERMISSION_NONE).equals(PERMISSION_READ);
@@ -68,14 +75,7 @@ public class StreamMetadata {
     }
 
 
-    //not using enums to easier serialization
-    public static final int PERMISSION_NONE = 0;
-    public static final int PERMISSION_READ = 1;
-    public static final int PERMISSION_WRITE = 2;
 
-    public static final int STREAM_ACTIVE = 0;
-    public static final int STREAM_LOCKED = 1;
-    public static final int STREAM_DELETED = 2;
 
 }
 
