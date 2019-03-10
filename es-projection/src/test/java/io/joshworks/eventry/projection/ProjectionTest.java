64be075f9f017dc62818c630132fe77958864e0b
Josh <josue.eduardo206@gmail.com>
2019-03-10 18:34:16 +0000

    Ignored projection tests

--- es-projection/src/test/java/io/joshworks/eventry/projection/ProjectionTest.java
+++ es-projection/src/test/java/io/joshworks/eventry/projection/ProjectionTest.java
@@ -22,7 +22,7 @@
 //import static org.junit.Assert.assertFalse;
 //import static org.junit.Assert.assertTrue;
 //
-//public class ProjectionIT {
+//public class ProjectionTest {
 //
 //    private File directory;
 //    private EventStore store;
