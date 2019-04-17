26553380ef00c110e4563c3ee2466ce22da0a1f8
Josh <josue.eduardo206@gmail.com>
2019-04-17 02:05:08 +0100

    Added blockWrapper and entry size validation to block

---
+++ logstore/src/main/java/io/joshworks/fstore/log/appender/BlockAppender.java
@@ -0,0 +1,7 @@
+package io.joshworks.fstore.log.appender;
+
+public class BlockAppender extends LogAppender {
+    BlockAppender(Config config) {
+        super(config);
+    }
+}
---
+++ logstore/src/main/java/io/joshworks/fstore/log/segment/block/BlockSegmentWrapper.java
@@ -0,0 +1,136 @@
+package io.joshworks.fstore.log.segment.block;
+
+import io.joshworks.fstore.log.Direction;
+import io.joshworks.fstore.log.SegmentIterator;
+import io.joshworks.fstore.log.segment.Log;
+import io.joshworks.fstore.log.segment.SegmentState;
+import io.joshworks.fstore.log.segment.header.Type;
+
+class BlockSegmentWrapper<T> implements Log<T> {
+
+    private final BlockSegment<T> delegate;
+
+    public BlockSegmentWrapper(BlockSegment<T> delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public long fileSize() {
+        return delegate.fileSize();
+    }
+
+    @Override
+    public long logSize() {
+        return delegate.logSize();
+    }
+
+    @Override
+    public long remaining() {
+        return delegate.remaining();
+    }
+
+    @Override
+    public String name() {
+        return delegate.name();
+    }
+
+    @Override
+    public SegmentIterator<T> iterator(Direction direction) {
+        return delegate.entryIterator(direction);
+    }
+
+    @Override
+    public SegmentIterator<T> iterator(long position, Direction direction) {
+        return delegate.entryIterator(position, direction);
+    }
+
+    @Override
+    public long position() {
+        return delegate.position();
+    }
+
+    @Override
+    public T get(long position) {
+        throw new UnsupportedOperationException("Operation not allowed in block segments");
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+    @Override
+    public SegmentState rebuildState(long lastKnownPosition) {
+        return delegate.rebuildState(lastKnownPosition);
+    }
+
+    @Override
+    public void delete() {
+        delegate.delete();
+    }
+
+    @Override
+    public void roll(int level) {
+        delegate.roll(level);
+    }
+
+    @Override
+    public boolean readOnly() {
+        return delegate.readOnly();
+    }
+
+    @Override
+    public boolean closed() {
+        return delegate.closed();
+    }
+
+    @Override
+    public long entries() {
+        return delegate.entries();
+    }
+
+    @Override
+    public int level() {
+        return delegate.level();
+    }
+
+    @Override
+    public long created() {
+        return delegate.created();
+    }
+
+    @Override
+    public long uncompressedSize() {
+        return delegate.uncompressedSize();
+    }
+
+    @Override
+    public Type type() {
+        return delegate.type();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return delegate.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return delegate.toString();
+    }
+
+    @Override
+    public long append(T data) {
+        return delegate.add(data);
+    }
+
+    @Override
+    public void flush() {
+        delegate.flush();
+    }
+}
