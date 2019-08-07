9df48f2d96ebc9b9742ed1b870c6d0681ab46349
Josh <josue.eduardo206@gmail.com>
2019-08-07 01:54:35 +0100

    Reworking ClusterStore

--- es-server/src/main/java/io/joshworks/eventry/server/cluster/node/Node.java
+++ es-server/src/main/java/io/joshworks/eventry/server/cluster/node/Node.java
@@ -5,23 +5,16 @@ import io.joshworks.eventry.api.IEventStore;
 import java.io.Closeable;
 import java.util.Objects;
 
-public class Partition implements Closeable {
+public class Node implements Closeable {
 
     private final String id;
-    private final String nodeId;
-    private boolean master;
     private final IEventStore store;
 
     public Status status; //TODO use, lock etc..
 
-    public Partition(String id, String nodeId, IEventStore store) {
+    public Node(String id, IEventStore store) {
         this.id = id;
-        this.nodeId = nodeId;
         this.store = store;
-    }
-
-    public boolean master() {
-        return master;
     }
 
     public IEventStore store() {
@@ -30,10 +23,6 @@ public class Partition implements Closeable {
 
     public String id() {
         return id;
-    }
-
-    public String nodeId() {
-        return nodeId;
     }
 
     public Status status() {
@@ -49,15 +38,14 @@ public class Partition implements Closeable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Partition partition = (Partition) o;
-        return id == partition.id &&
-                master == partition.master &&
-                Objects.equals(store, partition.store) &&
-                status == partition.status;
+        Node node = (Node) o;
+        return id == node.id &&
+                Objects.equals(store, node.store) &&
+                status == node.status;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, master, store, status);
+        return Objects.hash(id, store, status);
     }
 }
