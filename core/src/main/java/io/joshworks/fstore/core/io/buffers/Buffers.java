7911235fe36ece17d570c45f446184e52b88ace3
Josh <josue.eduardo206@gmail.com>
2020-01-06 21:01:37 +0000

    Added copy to Buffers

--- core/src/main/java/io/joshworks/fstore/core/io/buffers/Buffers.java
+++ core/src/main/java/io/joshworks/fstore/core/io/buffers/Buffers.java
@@ -1,5 +1,6 @@
 package io.joshworks.fstore.core.io.buffers;
 
+import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 
 import static io.joshworks.fstore.core.io.MemStorage.MAX_BUFFER_SIZE;
@@ -20,11 +21,39 @@ public class Buffers {
         //do not use src.remaining() here as we don't expect flipped buffers
         int srcRemaining = src.position() - srcPos;
         int readableBytes = Math.min(srcRemaining, dst.remaining());
-        int srcEndPos = srcPos + readableBytes;
-        for (int i = srcPos; i < srcEndPos; i++) {
-            dst.put(src.get(i));
-        }
+        copy(src, srcPos, readableBytes, dst);
         return readableBytes;
+    }
+
+    public static void copy(ByteBuffer src, int srcStart, int count, ByteBuffer dst) {
+        if (dst.remaining() < count) {
+            throw new BufferOverflowException();
+        }
+
+        int i = 0;
+        while ((count - i) >= Long.BYTES) {
+            dst.putLong(src.getLong(srcStart + i));
+            i += Long.BYTES;
+        }
+        while ((count - i) >= Integer.BYTES) {
+            dst.putInt(src.getInt(srcStart + i));
+            i += Integer.BYTES;
+        }
+        while ((count - i) >= Short.BYTES) {
+            dst.putShort(src.getShort(srcStart + i));
+            i += Short.BYTES;
+        }
+        while ((count - i) >= Byte.BYTES) {
+            dst.put(src.get(srcStart + i));
+            i += Byte.BYTES;
+        }
+    }
+
+    public static void copy(ByteBuffer src, ByteBuffer dst) {
+        if (src.remaining() > dst.remaining()) {
+            throw new BufferOverflowException();
+        }
+        copy(src, src.position(), src.remaining(), dst);
     }
 
     public static void offsetPosition(ByteBuffer data, int offset) {
