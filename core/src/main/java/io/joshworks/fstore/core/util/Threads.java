8f65c56b0b047a0084f019bf51dc44c9d51bc3a6
Josh Gontijo <josh.gontijo@oracle.com>
2022-04-05 18:20:51 +0100

    Minor fixes

--- core/src/main/java/io/joshworks/fstore/core/util/Threads.java
+++ core/src/main/java/io/joshworks/fstore/core/util/Threads.java
@@ -6,31 +6,48 @@ import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Threads {
+
+    private static AtomicInteger counter = new AtomicInteger();
+    public static final Thread.UncaughtExceptionHandler UNCAUGHT_EXCEPTION_HANDLER = (t, e) -> {
+        System.err.println("UncaughtException on " + t.getName() + ": " + e.getMessage());
+        e.printStackTrace(System.err);
+    };
+
     private Threads() {
 
     }
 
+    public static Thread spawn(Runnable runnable) {
+        return spawn("thread-" + counter.getAndIncrement(), runnable);
+    }
+
     public static Thread spawn(String name, Runnable runnable) {
-        var thread = thread(name, false, runnable);
+        var thread = thread(name, false, runnable, UNCAUGHT_EXCEPTION_HANDLER);
         thread.start();
         return thread;
     }
 
-    public static Thread named(String name, Runnable runnable) {
+    public static Thread thread(String name, Runnable runnable) {
         return thread(name, false, runnable);
     }
 
     public static Thread thread(String name, boolean daemon, Runnable runnable) {
+        return thread(name, daemon, runnable, UNCAUGHT_EXCEPTION_HANDLER);
+    }
+
+    public static Thread thread(String name, boolean daemon, Runnable runnable, Thread.UncaughtExceptionHandler handler) {
         Thread thread = new Thread(runnable, name);
         thread.setDaemon(daemon);
+        thread.setUncaughtExceptionHandler(handler);
         return thread;
     }
 
     public static ThreadFactory namedThreadFactory(String name) {
-        return r -> Threads.named(name, r);
+        return r -> Threads.thread(name, r);
     }
 
     public static ThreadFactory namePrefixedThreadFactory(String name) {
@@ -40,7 +57,7 @@ public class Threads {
 
             @Override
             public Thread newThread(Runnable r) {
-                return Threads.named(name + "-" + counter.getAndIncrement(), r);
+                return Threads.thread(name + "-" + counter.getAndIncrement(), r);
             }
         };
     }
@@ -55,6 +72,17 @@ public class Threads {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             throw new RuntimeException(e);
+        }
+    }
+
+    public static void waitFor(Thread... threads) {
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                thread.interrupt();
+                e.printStackTrace();
+            }
         }
     }
 
